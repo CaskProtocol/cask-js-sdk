98916bdc3ca2bc4942cf4dbbbea3cf62c814169e
mike <masha256bit@protonmail.com>
2022-05-25 17:08:12 -0700

    include code based discount in example

--- examples/subscriptionPlans.js
+++ examples/subscriptionPlans.js
@@ -45,6 +45,11 @@ async function publishProfile() {
     });
 
     providerProfile.setDiscount({
+        discountCode: 'SUMMER20',
+        value: 2000, // 20%,
+    });
+
+    providerProfile.setDiscount({
         description: "Save 50% when holding our token",
         discountERC20Address: '0x3f249f398D5aa6f528B97293ff8EDc2e0E0Ca54B', // mockERC20 dev token
         discountERC20Decimals: 18,
