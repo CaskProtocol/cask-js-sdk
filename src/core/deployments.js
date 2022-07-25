baf4249a796554314f8d3b78fe154d92d6bdb8c8
mike <masha256bit@protonmail.com>
2022-07-25 13:16:53 -0700

    bump version and enable avalanche

--- src/core/deployments.js
+++ src/core/deployments.js
@@ -24,7 +24,7 @@ const deployments = {
         },
         [environments.PRODUCTION]: {
             [chains.POLYGON_MAINNET.chainId]: '0x4A6f232552E0fd76787006Bb688bFBCB931cc3d0',
-            // [chains.AVAX_MAINNET.chainId]: '0x4A6f232552E0fd76787006Bb688bFBCB931cc3d0',
+            [chains.AVAX_MAINNET.chainId]: '0x4A6f232552E0fd76787006Bb688bFBCB931cc3d0',
         },
         [environments.DEVELOPMENT]: {
             [chains.ETH_HARDHAT.chainId]: '0xb651068Ace267cc30Ca77c91284772AD8315B4E6',
@@ -46,7 +46,7 @@ const deployments = {
         },
         [environments.PRODUCTION]: {
             [chains.POLYGON_MAINNET.chainId]: '0x78B5882b81AF02ebb0803eAFb4A4bf27fe35470e',
-            // [chains.AVAX_MAINNET.chainId]: '0x78B5882b81AF02ebb0803eAFb4A4bf27fe35470e',
+            [chains.AVAX_MAINNET.chainId]: '0x78B5882b81AF02ebb0803eAFb4A4bf27fe35470e',
         },
         [environments.DEVELOPMENT]: {
             [chains.ETH_HARDHAT.chainId]: '0xA80D4D3d0b2C9c38912459F2E674416973a9A161',
@@ -68,7 +68,7 @@ const deployments = {
         },
         [environments.PRODUCTION]: {
             [chains.POLYGON_MAINNET.chainId]: '0x3b2b4b547dAEEbf3A703288CB43650f0F287b9ff',
-            // [chains.AVAX_MAINNET.chainId]: '0x3b2b4b547dAEEbf3A703288CB43650f0F287b9ff',
+            [chains.AVAX_MAINNET.chainId]: '0x3b2b4b547dAEEbf3A703288CB43650f0F287b9ff',
         },
         [environments.DEVELOPMENT]: {
             [chains.ETH_HARDHAT.chainId]: '0x61E53693d81A38e72AAe0B2aC5f9B2dbb2B5ac56',
@@ -130,7 +130,7 @@ const deployments = {
         [environments.PRODUCTION]: {
             [chains.ETH_MAINNET.chainId]: '0x0645314c1Afd4d9711d609e3e755265aBaBc95Eb',
             [chains.POLYGON_MAINNET.chainId]: '0xdc185ade9a3362f9203191f136499ffe512527c1',
-            // [chains.AVAX_MAINNET.chainId]: '0x0000000000000000000000000000000000000000',
+            [chains.AVAX_MAINNET.chainId]: '0x0000000000000000000000000000000000000000',
         },
         [environments.DEVELOPMENT]: {
             [chains.ETH_HARDHAT.chainId]: '0x0000000000000000000000000000000000000000',
@@ -147,7 +147,7 @@ const deployments = {
         },
         [environments.PRODUCTION]: {
             [chains.POLYGON_MAINNET.chainId]: '0x0000000000000000000000000000000000000000',
-            // [chains.AVAX_MAINNET.chainId]: '0x0000000000000000000000000000000000000000',
+            [chains.AVAX_MAINNET.chainId]: '0x0000000000000000000000000000000000000000',
         },
         [environments.DEVELOPMENT]: {
             [chains.ETH_HARDHAT.chainId]: '0x0000000000000000000000000000000000000000',
@@ -167,7 +167,7 @@ const deployments = {
         },
         [environments.PRODUCTION]: {
             [chains.POLYGON_MAINNET.chainId]: "https://api.thegraph.com/subgraphs/name/caskprotocol/cask-protocol-polygon",
-            // [chains.AVAX_MAINNET.chainId]: "https://api.thegraph.com/subgraphs/name/caskprotocol/cask-protocol-avalanche",
+            [chains.AVAX_MAINNET.chainId]: "https://api.thegraph.com/subgraphs/name/caskprotocol/cask-protocol-avalanche",
         },
         [environments.DEVELOPMENT]: {
             [chains.ETH_HARDHAT.chainId]: null,
